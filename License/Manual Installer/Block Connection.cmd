64f6dda158e7dbbcdd2c192b1a40cf51f174988d
febry2402 <kepobanget053@gmail.com>
2022-08-28 02:10:38 +0700

    first commit

---
+++ License/Manual Installer/Block Connection.cmd
@@ -0,0 +1,6 @@
+attrib -r "C:\WINDOWS\system32\drivers\etc\hosts"
+echo
+echo # Start TablePlus Api Connection Block >> %windir%\system32\drivers\etc\hosts
+echo 0.0.0.0 api.tableplus.com >> %windir%\system32\drivers\etc\hosts
+echo # End TablePlus Api Connection Block >> %windir%\system32\drivers\etc\hosts
+echo
